48f5b083f00083e9941fb8b88bb9d0848c0eb3be
claudiu codoban <claudiu.codoban@deventure.co>
2016-05-13 00:16:37 +0300

    Updated the entities and some of the relations

--- TimisComplaints.Database/dbo/Tables/Letters.sql
+++ TimisComplaints.Database/dbo/Tables/Letters.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[Letters] (
     [Id]      UNIQUEIDENTIFIER NOT NULL,
     [UserId]  UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(200) NOT NULL, 
     [Message] NVARCHAR (MAX)   NOT NULL,
+    [Date] DATETIME NOT NULL, 
     CONSTRAINT [PK_Letters] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Letters_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
 );
--- TimisComplaints.Database/dbo/Tables/User.sql
+++ TimisComplaints.Database/dbo/Tables/User.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[User] (
     [Id]       UNIQUEIDENTIFIER NOT NULL,
-    [Email]    NVARCHAR (100)   NOT NULL,
-    [Password] NVARCHAR (50)    NOT NULL,
-    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Email]    NVARCHAR (100)   NULL,
+    [Password] NVARCHAR (50)    NULL,
+    [FirstName] NVARCHAR(100) NULL, 
+    [LastName] NVARCHAR(100) NULL, 
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
