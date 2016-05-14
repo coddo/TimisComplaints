65f5c8c09888e237e3f97f527bfbe32b51390f4d
raduaron26 <radu.aron26@gmail.com>
2016-05-14 20:32:56 +0300

    Added Web Api action which gets all problems for a given district

--- TimisComplaints.BusinessLogicLayer/Core/DistrictCore.cs
+++ TimisComplaints.BusinessLogicLayer/Core/DistrictCore.cs
@@ -1,6 +1,19 @@
-﻿namespace TimisComplaints.BusinessLogicLayer.Core
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimisComplaints.DataLayer;
+using TimisComplaints.DataLayer.Repositories;
+
+namespace TimisComplaints.BusinessLogicLayer.Core
 {
     public static class DistrictCore
     {
+        public static async Task<IList<Problem>> GetProblemsAsync(Guid id)
+        {
+            using (var districtRepository = new DistrictRepository())
+            {
+                return await districtRepository.GetProblemsAsync(id);
+            }
+        }
     }
 }
---
+++ TimisComplaints.WebApi/Controllers/DistrictController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TimisComplaints.BusinessLogicLayer.Core;
+
+namespace TimisComplaints.WebApi.Controllers
+{
+    public class DistrictController : ApiController
+    {
+        [HttpGet]
+        [ActionName("GetProblems/{id}")]
+        public async Task<IHttpActionResult> GetProblemsAsync(Guid id)
+        {
+            try
+            {
+                var result = await DistrictCore.GetProblemsAsync(id);
+                if (result == null)
+                {
+                    return BadRequest("No problems found");
+                }
+
+                return Ok(result);
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        } 
+    }
+}
