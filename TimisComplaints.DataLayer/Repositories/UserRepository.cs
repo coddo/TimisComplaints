59c679a062de56cb1d9ae9dcef87a70164f8bf16
raduaron26 <radu.aron26@gmail.com>
2016-05-16 00:27:20 +0300

    Added action to get all problems for a user

--- TimisComplaints.DataLayer/Repositories/UserRepository.cs
+++ TimisComplaints.DataLayer/Repositories/UserRepository.cs
@@ -1,8 +1,21 @@
-﻿using TimisComplaints.DataLayer.Repositories.Base;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimisComplaints.DataLayer.Repositories.Base;
 
 namespace TimisComplaints.DataLayer.Repositories
 {
     public class UserRepository : BaseRepository<User>
     {
+        public async Task<IList<UserProblem>> GetUserProblemsAsync(Guid id)
+        {
+            var user = await GetAsync(id, new[]
+            {
+                "UserProblems.Problem"
+            });
+
+            return user.UserProblems.ToList();
+        }
     }
 }
