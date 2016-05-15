9d37894f4a6749a1bbecbbb543cc564ba0ec2bb9
raduaron26 <radu.aron26@gmail.com>
2016-05-16 00:57:43 +0300

    Added action to get the user id based on the session key

---
+++ TimisComplaints.DataLayer/Repositories/SessionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using TimisComplaints.DataLayer.Repositories.Base;
+
+namespace TimisComplaints.DataLayer.Repositories
+{
+    public class SessionRepository : BaseRepository<Session>
+    {
+        public async Task<User> GetUserAsync(string key)
+        {
+            var session = await FetchSingleAsync(s => s.Key == key, new []
+            {
+                nameof(Session.User)
+            });
+
+            return session?.User;
+        }
+    }
+}
