d646d7efdb9a2fe1c5681cce6c18b8210b96b975
Coddo <codobanclaudiu@gmail.com>
2016-05-17 01:37:55 +0300

    Added functionality for getting and creating letters

--- TimisComplaints.WebApi/Controllers/LetterController.cs
+++ TimisComplaints.WebApi/Controllers/LetterController.cs
@@ -1,8 +1,48 @@
-﻿using TimisComplaints.WebApi.Controllers.Base;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TimisComplaints.BusinessLogicLayer.Core;
+using TimisComplaints.DataLayer;
+using TimisComplaints.WebApi.Controllers.Base;
+using TimisComplaints.WebApi.Models;
 
 namespace TimisComplaints.WebApi.Controllers
 {
     public class LetterController : IdentityInjectedController
     {
+        [HttpGet]
+        [ActionName("GetAll")]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            var letters = await LetterCore.GetAllAsync();
+
+            return Ok(letters);
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public async Task<IHttpActionResult> Create([FromBody] LetterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var letter = new Letter
+            {
+                UserId = Identity.Id,
+                Date = DateTime.Now,
+                Message = model.Message,
+                Title = model.Title
+            };
+
+            letter = await LetterCore.CreateAsync(letter);
+            if (letter == null)
+            {
+                return InternalServerError();
+            }
+
+            return Ok(letter);
+        }
     }
 }
