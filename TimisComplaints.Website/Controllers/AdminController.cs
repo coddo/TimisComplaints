48137eef2fd4861afcd0547f641919cbe285d2db
Coddo <codobanclaudiu@gmail.com>
2016-06-02 01:42:20 +0300

    Added admin controller with credentials

---
+++ TimisComplaints.Website/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TimisComplaints.Website.Controllers
+{
+    public class AdminController : ApiController
+    {
+        private const string USERNAME = "Botici";
+        private const string PASSWORD = "Timis#123Complaints@";
+
+        [HttpGet]
+        [ActionName("Authenticate")]
+        public IHttpActionResult Authenticate(string username, string password)
+        {
+            if (username != USERNAME || password != PASSWORD)
+            {
+                return Unauthorized();
+            }
+
+            return Ok();
+        }
+    }
+}
