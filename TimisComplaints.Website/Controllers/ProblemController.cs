09b4ee08a4f1b9355b85db45f88534f0fa38b734
Coddo <codobanclaudiu@gmail.com>
2016-06-04 01:08:56 +0300

    Added extra validations for when creating problems

--- TimisComplaints.Website/Controllers/ProblemController.cs
+++ TimisComplaints.Website/Controllers/ProblemController.cs
@@ -100,7 +100,7 @@ namespace TimisComplaints.Website.Controllers
         {
             try
             {
-                if (!ModelState.IsValid)
+                if (!ModelState.IsValid || string.IsNullOrWhiteSpace(model.Name) || string.IsNullOrWhiteSpace(model.Description))
                 {
                     return BadRequest(ModelState);
                 }
